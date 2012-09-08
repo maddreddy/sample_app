8fa36642b20c47dbf33f4f90722ee94bf6b663bc
Madhav Reddy <madhav@mayaclinicals.com>
2012-09-08 16:04:24 -0700

    Add user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,6 +4,26 @@ describe "Static pages" do
 
   subject { page }
 
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+        FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
+
+		end
+
+
+
+
   describe "Home page" do
     before { visit root_path }
 
